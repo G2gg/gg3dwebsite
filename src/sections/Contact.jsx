98365ad14634bf825a89c0781f95b48ed04da68a
Gunesh Gupta <112479039+G2gg@users.noreply.github.com>
2025-04-25 20:57:51 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -74,8 +74,7 @@ const Contact = () => {
         <div className="contact-container">
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
-            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-            life, I’m here to help.
+            To hire me as a research intern, or to ask about my projects, please send the message from here.
           </p>
 
           <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
